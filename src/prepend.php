2611cb66d9e09a6aeabf5e4cccd71c79b2bd84e8
Bruce Weirdan <weirdan@gmail.com>
2018-03-05 15:41:22 +0200

    allow to be installed globally (path fixes)

--- src/prepend.php
+++ src/prepend.php
@@ -1,4 +1,10 @@
 <?php
 namespace Weirdan\RunWithoutXdebug;
-require __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    // standalone
+    require __DIR__ . '/../vendor/autoload.php';
+} else if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+    // global or project
+    require __DIR__ . '/../../../vendor/autoload.php';
+}
 (new \Composer\XdebugHandler\XdebugHandler('RUNWITHOUTXDEBUG'))->check();
