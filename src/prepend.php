8b4a560aac55457ffb26351742e9886591eb863d
Bruce Weirdan <weirdan@gmail.com>
2018-04-24 11:45:52 +0300

    Fixes composer/xdebug-handler:1.1 breaks
    Requires (currently)
    https://github.com/johnstevenson/xdebug-handler/tree/standard-input
    
    Merge after composer/xdebug-handler#59 is merged and released. Drop
    `repositories` and adjust the branch.

--- src/prepend.php
+++ src/prepend.php
@@ -7,4 +7,25 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     // global or project
     require __DIR__ . '/../../../autoload.php';
 }
-(new \Composer\XdebugHandler\XdebugHandler('RUNWITHOUTXDEBUG'))->check();
+
+call_user_func(function() {
+    $x = new \Composer\XdebugHandler\XdebugHandler('RWX');
+    if ($logFile = getenv('RWX_DEBUG_LOG')) {
+        $logger = new \Monolog\Logger('rwx', [new \Monolog\Handler\StreamHandler($logFile)]);
+    } else {
+        $logger = new \Psr\Log\NullLogger;
+    }
+
+    // we don't have to detect command line code as auto_prepend_file doesn't work in that case
+    $mainScript = in_array($_SERVER['argv'][0], ['-', 'Standard input code'], true)
+                    ? '--'
+                    : $_SERVER['argv'][0];
+
+    $logger->debug('argv[0]: ' . $_SERVER['argv'][0]);
+    $logger->debug('Main script: ' . $mainScript);
+
+    $x->setMainScript($mainScript);
+
+    $x->setLogger($logger);
+    $x->check();
+});
