9bd7ce265fffda9759427fbada7c0443de5f6a48
Bruce Weirdan <weirdan@gmail.com>
2020-08-25 03:42:14 +0300

    Drop debugging statements and fix CS

--- src/Restarter.php
+++ src/Restarter.php
@@ -11,8 +11,11 @@ class Restarter extends XdebugHandler
     {
         assert(null !== $this->tmpIni);
         $contents = file_get_contents($this->tmpIni);
-        $contents = preg_replace('/^auto_prepend_file="' . preg_quote(__DIR__ . '/prepend.php', '/') . '"/m', '', $contents, -1, $count);
-        var_dump($count);
+        $contents = preg_replace(
+            '/^auto_prepend_file="' . preg_quote(__DIR__ . '/prepend.php', '/') . '"/m',
+            '',
+            $contents
+        );
         file_put_contents($this->tmpIni, $contents);
         parent::restart($command);
     }
